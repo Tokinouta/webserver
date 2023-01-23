8337000cb8f113903ca6dd01d2a0a138dfc3473e
Hou Boyao <shangrila-1100@hotmail.com>
2023-01-23 23:00:00 +0800

    added -fexperimental-library to enable <format>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <format>
 #include <iostream>
+#include <string>
 
 #include "webserver.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
   std::cout << "hello world!" << std::endl;
+  if (argc <= 2) {
+    std::cout << std::format("Usage: {} <ip address> <port>", argv[0]) << std::endl;
+    return 1;
+  }
+  auto ip{argv[1]};
+  auto port{atoi(argv[2])};
   auto a{webserver(10)};
   a.print();
 }
